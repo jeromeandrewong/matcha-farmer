3924926aab61a040a4dff50e8d0d00d21d9cb628
Jerome <jeromewongjh@gmail.com>
2024-10-26 19:35:19 +0800

    feat: add eventbridge cron job

--- deployment/main.tf
+++ deployment/main.tf
@@ -34,3 +34,23 @@ resource "aws_lambda_function" "matcha-farmer" {
     }
   }
 }
+
+resource "aws_cloudwatch_event_rule" "matcha_farmer_schedule" {
+  name                = "matcha-farmer-schedule"
+  description         = "Schedule for Matcha Farmer Lambda Function"
+  schedule_expression = "cron(0 4,16 * * ? *)"# every 12 hours at midnight and noon SGT
+}
+
+resource "aws_cloudwatch_event_target" "matcha_farmer_target" {
+  rule      = aws_cloudwatch_event_rule.matcha_farmer_schedule.name
+  target_id = "matcha-farmer"
+  arn       = aws_lambda_function.matcha-farmer.arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.matcha-farmer.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.matcha_farmer_schedule.arn
+}
